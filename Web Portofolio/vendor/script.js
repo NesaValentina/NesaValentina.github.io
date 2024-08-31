ce355fcd9276a627329d1523a11ae32938b65124
NesaValentina <tentanglangit914@gmail.com>
2024-08-31 13:48:39 +0700

    Add files via upload

---
+++ Web Portofolio/vendor/script.js
@@ -0,0 +1,9 @@
+var typed = new Typed("#element", {
+    strings: [
+        "Web Developer",
+        "Web Designer",
+        "Freelancer",
+        "UI/UX Designer",
+    ],
+    typeSpeed:100,
+})
