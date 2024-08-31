02a788642c2b4fe0f17c600cba5cee9b9268eecc
NesaValentina <tentanglangit914@gmail.com>
2024-08-31 13:51:02 +0700

    Delete Web Portofolio directory

--- Web Portofolio/vendor/script.js
+++
@@ -1,9 +0,0 @@
-var typed = new Typed("#element", {
-    strings: [
-        "Web Developer",
-        "Web Designer",
-        "Freelancer",
-        "UI/UX Designer",
-    ],
-    typeSpeed:100,
-})
